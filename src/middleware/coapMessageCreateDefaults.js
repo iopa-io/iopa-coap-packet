613c1b4b14fb7bfc9310a185399e0b746accf6a6
guycreate <admin@domabo.com>
2015-08-30 11:17:37 -0500

    writeack

--- src/middleware/coapMessageCreateDefaults.js
+++ src/middleware/coapMessageCreateDefaults.js
@@ -36,7 +36,7 @@ function CoAPMessageCreateDefaults(app) {
 
 CoAPMessageCreateDefaults.prototype.invoke = function CoAPMessageCreateDefaults_invoke(context, next){
      context["server.CreateRequest"] = CoAPMessageCreateDefaults_createRequest.bind(this, context["server.CreateRequest"]);
-     context["server.WriteAck"] = CoAPFormat.WriteAck.bind(this, context);
+     context["coap.WriteAck"] = CoAPFormat.WriteAck.bind(this, context.response);
      return next();
 };
 
