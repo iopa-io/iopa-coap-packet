c0d8644230a57f1b76f365edf2d66ec31002282a
guycreate <guycreate@users.noreply.github.com>
2015-10-09 17:01:07 -0500

    dev test

--- src/common/coapFormat.js
+++ src/common/coapFormat.js
@@ -349,7 +349,7 @@ function _coapSendResponse(context, payload) {
   var buf = coapPacket.generate(packet);
   response[SERVER.RawStream].write(buf);
 
-  // response[IOPA.MessageId] = null;
+   response[IOPA.MessageId] = null;
 }
 
 /**
